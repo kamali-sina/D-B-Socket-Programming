5795ca497385b0c289cfc5a3cc80c3db54f41937
Sina Kamali <Sina Kamali>
2020-10-17 13:45:06 +0330

    it can now recieve messages from any connection

--- server.c
+++ server.c
@@ -3,14 +3,21 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/select.h>
 #include <sys/time.h>
 #include <string.h>
-
+#include <unistd.h>
+#include <fcntl.h> 
+#include "message_handler.h"
 
 #define SERVER_IPV4_ADDR "127.0.0.1"
 #define MAX_CLIENTS 20
+#define STDIN_FILENO 0
 #define NO_SOCKET -1
+
+char server_name[30] = "server";
 
 void DieWithError(char *errorMessage)
 {
@@ -18,16 +25,15 @@ void DieWithError(char *errorMessage)
     exit(1);
 }
 
-typedef struct {
-    int socket;
-    struct sockaddr_in address;
-    size_t current_sending_byte;
-    size_t current_receiving_byte;
-} peer_t;
+connection connection_list[MAX_CLIENTS];
 
-peer_t connection_list[MAX_CLIENTS];
+void init_connections(){
+    for (int i = 0; i < MAX_CLIENTS; i++){
+        connection_list[i].socket = NO_SOCKET;
+    }
+}
 
-int start_listen_socket(int *listen_sock, int listen_port){
+int start_listen_socket(int *listen_sock, int listen_port, int logger_fd){
     *listen_sock = socket(AF_INET, SOCK_STREAM, 0);
     if (*listen_sock < 0) {
         perror("socket");
@@ -42,18 +48,25 @@ int start_listen_socket(int *listen_sock, int listen_port){
         perror("bind");
         return -1;
     }
-    
-    // start accept client connections
+    // we use 10 because maximum number of people in queue will be 1+2+3 = 6
     if (listen(*listen_sock, 10) != 0) {
         perror("listen");
         return -1;
     }
-    printf("Accepting connections on port %d.\n", (int)listen_port);
+    char message[100];
+    snprintf(message, sizeof(message),"Accepting connections on port %d.\n", (int)listen_port);
+    logger_log(message, logger_fd);
     return 0;
 }
 
-int handle_new_connection(int listen_sock)
-{
+void close_client_connection(connection* peer, int logger_fd){
+    char message[100];
+    snprintf(message, sizeof(message),"closed connection on port %d\n", peer->address.sin_port);
+    logger_log(message,logger_fd);
+    peer->socket = NO_SOCKET;
+}
+
+int handle_new_connection(int listen_sock, int logger_fd){
     struct sockaddr_in client_addr;
     memset(&client_addr, 0, sizeof(client_addr));
     socklen_t client_len = sizeof(client_addr);
@@ -64,20 +77,39 @@ int handle_new_connection(int listen_sock)
     }
     char client_ipv4_str[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &client_addr.sin_addr, client_ipv4_str, INET_ADDRSTRLEN);
-    printf("Incoming connection from %s:%d.\n", client_ipv4_str, client_addr.sin_port);
-    int i;
-    for (i = 0; i < MAX_CLIENTS; ++i) {
+    char message [100];
+    snprintf(message, sizeof(message), "Incoming connection from %s:%d.\n", client_ipv4_str, client_addr.sin_port);
+    logger_log(message, logger_fd);
+    for (int i = 0; i < MAX_CLIENTS; i++) {
         if (connection_list[i].socket == NO_SOCKET) {
             connection_list[i].socket = new_client_sock;
             connection_list[i].address = client_addr;
-            connection_list[i].current_sending_byte   = -1;
-            connection_list[i].current_receiving_byte = 0;
+            connection_list[i].sending_something_to = 0;
             return 0;
         }
     }
-    char buf[20] = "boos";
-    send(new_client_sock, &buf, 5, 0);
-    printf("sent something\n");
+    logger_log("could not save the new client.\n", logger_fd);
+    return -1;
+}
+
+int reset_fds(fd_set *read_fds, fd_set *write_fds, int listen_sock){
+    int fdmax = listen_sock;
+    FD_ZERO(read_fds);
+    FD_SET(STDIN_FILENO, read_fds);
+    FD_SET(listen_sock, read_fds);
+    for (int i = 0; i < MAX_CLIENTS; i++){
+        if (connection_list[i].socket != NO_SOCKET){
+            FD_SET(connection_list[i].socket, read_fds);
+            if (connection_list[i].socket > fdmax)
+                fdmax = connection_list[i].socket;
+        }
+    }
+    FD_ZERO(write_fds);
+    for (int i = 0; i < MAX_CLIENTS; ++i)
+        if (connection_list[i].socket != NO_SOCKET)
+            FD_SET(connection_list[i].socket, write_fds);
+
+    return fdmax;
 }
 
 int main(int argc, char *argv[]){
@@ -85,38 +117,50 @@ int main(int argc, char *argv[]){
         printf("No Command Line Arguments were provided\n");
         exit(0);
     }
-
-
+    init_connections();
+    int logger_fd = logger_make("server_log.txt");
     int listen_port = atoi(argv[1]);
     int listen_sock;
-    if (start_listen_socket(&listen_sock, listen_port) == -1){
+    if (start_listen_socket(&listen_sock, listen_port, logger_fd) == -1){
+        logger_log("could not listen on given port\n", logger_fd);
         exit(0);
     }
 
     fd_set read_fds;
-    printf("this is listen_sock: %d\n",listen_sock);
+    fd_set write_fds;
     int fdmax = listen_sock;
 
-    while( 1 )
-    {
-        FD_ZERO(&read_fds);
-        FD_SET(listen_sock, &read_fds);
-        for (int i = 0; i < MAX_CLIENTS; i++)
-            FD_SET(connection_list[i].socket, &read_fds);
-
-        // for (int i = 0; i < fdmax+1; i++){
-        //     printf("status of %d is %d\n",i,FD_ISSET(i,&read_fds));
-        // }
+    while(1){
+        fdmax = reset_fds(&read_fds, &write_fds, listen_sock);
         int status = select(fdmax+1, &read_fds, NULL,NULL,NULL);
         if (status == -1){
-            printf("fuck\n");
+            logger_log("fuck, select status was -1\n",logger_fd);
             return -1;
         }else if (status == 0){
-            printf("you shouldnt have done that\n");
+            logger_log("you shouldnt have done that, select status was 0\n", logger_fd);
             return 0;
         }
         if (FD_ISSET(listen_sock,&read_fds)){
-            handle_new_connection(listen_sock);
+            if (handle_new_connection(listen_sock, logger_fd) != 0){
+                logger_log("Could not handle new connection, Terminating\n",logger_fd);
+                exit(0);
+            }
         }
+        for (int i = 0; i < MAX_CLIENTS; ++i) {
+            if (connection_list[i].socket != NO_SOCKET && FD_ISSET(connection_list[i].socket, &read_fds)) {
+                if (get_message_connection(&connection_list[i], logger_fd) != 0) {
+                    close_client_connection(&connection_list[i], logger_fd);
+                    continue;
+                }
+            }
+    
+            // if (connection_list[i].socket != NO_SOCKET && FD_ISSET(connection_list[i].socket, &write_fds)) {
+            //     if (send_to_peer(&connection_list[i]) != 0) {
+            //         close_client_connection(&connection_list[i], logger_fd);
+            //         continue;
+            //     }
+            // }
+        }
+
     }
 }
